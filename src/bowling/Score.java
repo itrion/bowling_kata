c7b38ef003a9baac07612c121b27b9e34dea6964
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2015-05-06 00:18:47 +0100

    calculates the total of a single frame as the sum of the first try and the second try

--- src/bowling/Score.java
+++ src/bowling/Score.java
@@ -2,21 +2,29 @@ package bowling;
 
 public class Score {
 	
-	private final String[] tries;
+	private final String[] framesScores;
 	
 	public Score(String frames) {
-		tries = frames.split(" ");
+		this.framesScores = frames.split(" ");
 	}
 
 	public int total() {
 		int sum = 0;
-		for (String aTry : tries) {
-			sum += calculateTry(aTry);
+		for (int frame = 0; frame < framesScores.length; frame++) {
+			sum += totalForFrame(frame);
 		}
 		return sum;
 	}
 	
-	private int calculateTry(String aTry) {
-		return new Integer(aTry.substring(0, 1)) + new Integer(aTry.substring(1, 2));
+	private int totalForFrame(int frame) {
+		return firstTryPunctuation(frame) + secondTryPunctuation(frame);
+	}
+
+	private Integer secondTryPunctuation(int frame) {
+		return new Integer(framesScores[frame].substring(1, 2));
+	}
+
+	private Integer firstTryPunctuation(int tryIndex) {
+		return new Integer(framesScores[tryIndex].substring(0, 1));
 	}
 }
