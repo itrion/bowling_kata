d50e76c8b670318c8c497fc17af45ef42ec3bc21
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2015-05-05 23:15:40 +0100

    score is zero when all frames are zero

---
+++ test/ScoreTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class ScoreTest {
+	
+	@Test
+	public void should_be_zero_when_all_frames_are_zero() {
+		String frames = "0 0 0 0 0 0 0 0 0 0 0 0";
+		assertThat(new Score(frames).total(), is(0));
+	}
+}
