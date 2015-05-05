13bd74760901992b541b16e27e5bf394f41b7612
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2015-05-05 23:31:48 +0100

    should be a flat sum of frames when no spare or strike

--- test/bowling/ScoreTest.java
+++ test/bowling/ScoreTest.java
@@ -9,7 +9,13 @@ public class ScoreTest {
 	
 	@Test
 	public void should_be_zero_when_all_frames_are_zero() {
-		String frames = "0 0 0 0 0 0 0 0 0 0 0 0";
+		String frames = "0 0 0 0 0 0 0 0 0 0";
 		assertThat(new Score(frames).total(), is(0));
+	}
+	
+	@Test
+	public void should_be_a_flat_sum_of_frames_when_no_spare_or_strike() {
+		String frames = "1 1 1 1 1 1 1 1 1 1";
+		assertThat(new Score(frames).total(), is(10));
 	}
 }
