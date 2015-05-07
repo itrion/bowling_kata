789516e87bcf3a6f37479f6fe2be90823fc99d16
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2015-05-07 01:42:39 +0100

    code cleaned

--- src/bowling/Score.java
+++ src/bowling/Score.java
@@ -2,6 +2,11 @@ package bowling;
 
 public class Score {
 	
+	private static final int MAX_FRAMES = 10;
+	private static final int STRIKE_POINTS = 20;
+	private static final int SPARE_POINTS = 10;
+	private static final int FIRST_FRAME = 0;
+	private static final int FLAT_TOTAL_WHEN_STRIKE_OR_SPARE = 10;
 	private final String[] framesScores;
 	
 	public Score(String frames) {
@@ -10,26 +15,29 @@ public class Score {
 	
 	public int total() {
 		int grandTotal = 0;
-		for (int frame = 0; frame < 10; frame++) {
-			grandTotal += totalForFrame(frame);
+		for (int frame = FIRST_FRAME; frame < MAX_FRAMES; frame++) {
+			grandTotal += total(frame);
 		}
 		return grandTotal;
 	}
 	
-	private int totalForFrame(int frame) {
-		if (isStrike(frame)) return 20 + baseTotalForFrame(frame + 1);
-		int totalForFrame = firstThrow(frame) + secondThrow(frame);
-		if (totalForFrame == 10) totalForFrame += baseTotalForFrame(frame + 1);
-		return totalForFrame;
+	private int total(int frame) {
+		if (isStrike(frame)) return STRIKE_POINTS + flatTotal(frame + 1);
+		if (isSpare(frame)) return SPARE_POINTS + flatTotal(frame + 1);
+		return flatTotal(frame);
 	}
-	
-	private int baseTotalForFrame(int frame) {
-		if (isStrike(frame)) return 10;
+
+	private int flatTotal(int frame) {
+		if (isStrike(frame) || isSpare(frame)) return FLAT_TOTAL_WHEN_STRIKE_OR_SPARE;
 		return firstThrow(frame) + secondThrow(frame);
 	}
 
 	private boolean isStrike(int frame) {
 		return framesScores[frame].equals("X");
+	}
+
+	private boolean isSpare(int frame) {
+		return firstThrow(frame) + secondThrow(frame) == 10;
 	}
 
 	private Integer firstThrow(int frame) {
